6f59a1b29e5a50f9759fb3525be487a4016f3f71
ArielKarp <akarpilovsky@gmail.com>
2018-06-05 17:41:28 +0300

    Client finished

--- src/pcc_client.c
+++ src/pcc_client.c
@@ -5,7 +5,7 @@
  *      Author: ariel
  */
 
-
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -18,6 +18,8 @@
 #include <errno.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
+
 
 
 int handle_error_exit(const char* error_msg) {
@@ -37,6 +39,17 @@ int isIPAddr(const char* input_str) {
 	return rc != 0;
 }
 
+unsigned int convertCharArrayToUsingedInt(char* input_arr, int size_of_arr, int* success) {
+	for(int i = 0; i < size_of_arr; ++i) {
+		if(!isdigit(input_arr[i])) {
+			*success = 0;
+			return 0;
+		}
+	}
+	*success = 1;
+	return (strtoul(input_arr, NULL, 10));
+}
+
 int main(int argc, char* argv[]) {
 	// get input from user
 	if (argc != 4) {
@@ -44,14 +57,20 @@ int main(int argc, char* argv[]) {
 	}
 
 	int sockfd = -1;
+	int rc = 0;
 
 	struct sockaddr_in serv_addr; // where we want to get to
-	struct sockaddr_in my_addr;   // where we actually connected through
-	struct sockaddr_in peer_addr; // where we actually connected to
 
 	char* server_host = argv[1];
-	int16_t server_port = atoi(argv[2]);
-	unsigned int len_to_read = atoi(argv[3]);
+	int16_t server_port = convertCharArrayToUsingedInt(argv[2], strlen(argv[2]), &rc);
+	if (!rc) {
+		handle_error_exit("Wrong port number");
+	}
+	rc = 0;
+	unsigned int len_to_read = convertCharArrayToUsingedInt(argv[3], strlen(argv[3]), &rc);
+	if (!rc) {
+		handle_error_exit("Wrong numer of bytes to read");
+	}
 
 	// get serv_addr ready
 	memset(&serv_addr, 0, sizeof(serv_addr));
@@ -60,7 +79,9 @@ int main(int argc, char* argv[]) {
 
 	// check if server host is a valid IP
 	if (isIPAddr(server_host)) {
-		inet_pton(AF_INET, input_str, &(serv_addr.sin_addr));
+		if (inet_pton(AF_INET, input_str, &(serv_addr.sin_addr)) != 1) { //; // TODO: check error
+			handle_error_exit("Error in inet_pton");
+		}
 	} else {
 		// this is a host name
 		// make this pretty
@@ -85,5 +106,61 @@ int main(int argc, char* argv[]) {
 		handle_error_exit("Failed to connect to server");
 	}
 
+	// open /dev/urandom
+	int urand_fd = open("/dev/urandom", O_RDONLY);
+	if (urand_fd < 0) {
+		handle_error_exit("Failed opening /dev/urandom for read");
+	}
+
+	// read len_of_read bytes from urandom
+	char* read_buf = calloc((len_to_read + 1), sizeof(char));
+
+	if (read_buf == NULL) {
+		handle_error_exit("Failed to allocate memory for read buffer");
+	}
+
+	// do a read
+	int bytes_read = read(urand_fd, read_buf, len_to_read);
+	if (bytes_read == -1) {
+		handle_error_exit("Failed reading from urandom");
+	}
+	bytes_read[len_to_read] = '\0';
+
+	// write to server
+	int wrote_bytes = 0;
+	while (wrote_bytes < len_to_read) {
+		int bytes_wrote = write(sockfd, read_buf + wrote_bytes, len_to_read - wrote_bytes);
+		if (bytes_wrote == -1) {
+			handle_error_exit("Failed to write to server");
+		}
+		wrote_bytes += bytes_wrote;
+
+	}
+
+	// read from server
+	int read_bytes = 0;
+	char read_server_buf[sizeof(unsigned int) + 1];
+	memset(read_server_buf, 0, sizeof(read_server_buf));
+	while (read_bytes < sizeof(unsigned int)) {
+		int done_read = read(sockfd, read_server_buf + read_bytes, sizeof(unsigned int) - read_bytes);
+		if (done_read == -1) {
+			handle_error_exit("Failed to read from server");
+		}
+		read_bytes += done_read;
+	}
+	read_server_buf[sizeof(unsigned int)] = '\0';
+
+	// convert read from server to unsinged int
+	rc = 0;
+	unsigned int C = convertCharArrayToUsingedInt(read_server_buf, sizeof(read_server_buf), &rc);
+
+	if(!rc) {
+		//TODO: what if server transmitted not a number
+	}
+
+	printf("# of printable characters: %u\n", C);
+
+
+	free(read_buf);
 	return EXIT_SUCCESS;
 }
